26b3505f0ba1d4ebce7fa43ede126d347bdd4d73
nile64 <87245996+nile64@users.noreply.github.com>
2024-09-03 15:12:59 -0400

    Update score.js

--- js/score.js
+++ js/score.js
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
+    let score = (-24.9975*Math.pow(rank-1, 0.4) + 500) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
