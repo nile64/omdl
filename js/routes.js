ae23ed6b8773e067a5a192a39a0fbadd271a4c3c
nile64 <87245996+nile64@users.noreply.github.com>
2024-09-03 09:14:28 -0400

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,11 @@
 import List from './pages/List.js';
+import ILL from './pages/ILL.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 
 export default [
     { path: '/', component: List },
+    { path: '/ill', component: ILL },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
 ];
