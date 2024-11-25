3b374dd7c06ee1551d00d860fdc45776362111e0
nile64 <87245996+nile64@users.noreply.github.com>
2024-11-25 09:21:44 -0500

    scoring changes + testing legacy

--- js/score.js
+++ js/score.js
@@ -11,10 +11,10 @@ const scale = 3;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 150) {
+    if (rank > 50) {
         return 0;
     }
-    if (rank > 75 && percent < 100) {
+    if (rank > 25 && percent < 100) {
         return 0;
     }
 
@@ -24,8 +24,16 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-24.9975*Math.pow(rank-1, 0.4) + 500) *
-        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
+    let score = 0;
+    
+    if(rank <= 25){
+        score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
+            ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
+    }
+    else {
+        score = (-24.9975*Math.pow(rank-1, 0.4) + 100) *
+            ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
+    }
 
     score = Math.max(0, score);
 
